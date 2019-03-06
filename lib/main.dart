a7e5eb6d9e5ed3463115e41c65c6e605cb979b16
Argel  Bejarano <argel.bc18@gmail.com>
2019-03-06 09:42:55 -0700

    First commit using the Flutter default template / usando el template por defecto

---
+++ lib/main.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'package:state_mgmt_example/src/app.dart';
+
+void main() => runApp(MyApp());
---
+++ lib/src/app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:state_mgmt_example/src/screens/myHomePage.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter State Management',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'State Management'),
+    );
+  }
+}
